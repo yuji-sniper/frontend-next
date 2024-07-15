38f311e9d97a835c47e4761c3380775c96b2b38a
吉村直人 <ahowateto123@gmail.com>
2024-07-15 10:10:53 +0900

    メニューのパスを変えた

--- client/app/components/header.tsx
+++ client/app/components/header.tsx
@@ -55,7 +55,7 @@ function Header() {
             variant="h6"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/"
             sx={{
               mr: 2,
               display: { xs: 'none', md: 'flex' },
